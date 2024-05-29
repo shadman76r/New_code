0da6e947c10c28d1c3d6bebbcd53864fab45fdd0
shadman76r <shadmansakibalam@gmail.com>
2024-05-30 01:41:29 +0600

    sum

--- first.cpp
+++ first.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 int main()
 {
-    cout<<"shadman";
+    int a=7;
+    int b=8;
+    //cin>>a>>b;
+    //cout<<"enter the two numbers";
+    cout<<"the sum of this two numbers are"<<a+b<<endl;
     return 0;
 }
