fbee5541bb6634e596bea2895d41424096de9e4e
shadman76r <shadmansakibalam@gmail.com>
2024-05-30 14:16:54 +0600

    leetcode loops problem 1

--- first.cpp
+++ first.cpp
@@ -1,11 +1,21 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    int a=7;
-    int b=8;
-    //cin>>a>>b;
-    //cout<<"enter the two numbers";
-    cout<<"the sum of this two numbers are"<<a+b<<endl;
-    return 0;
-}
+/// Number of stepts to reduce a number in binary represenrtaion to one
+
+class Solution{
+    public:
+    int numSteps(string  s)
+    {
+         int n = s.length();
+         int op=0;
+         int carry=0;
+          for(int i=n-1; i>=1; i--) // testing
+          {
+            if(((s[i]-'0')+carry)%2==1){ // this is for odd
+            op+=2;
+            carry=1;
+            }else{  // this is for even number
+                op+=1;
+            }
+          }
+          return op+carry; // main result
+    }
+};
