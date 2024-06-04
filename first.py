b074afae6975ac8b409252c5f02990b3bcc327dc
shadman76r <shadmansakibalam@gmail.com>
2024-06-05 01:49:22 +0600

    sum

--- first.py
+++ first.py
@@ -1,6 +1,16 @@
+#input
 a=input("Please Enter your name first to run the program")
 b=input("Please Enter your user name that us unique")
 print("Name:" + a)
-print("User_name" +b)
+print("User_name:" +b)
+
+#sum
+print("Now you can do some sum")
+c=input("Please Enter the number: ")
+d=input("Please Enter the another number: ")
+sum=int(c)+int(d)
+print(sum)
+
+
 
 
