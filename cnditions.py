3548ee29af657eb068a78c2e13b8874f27b8f02f
shadman76r <shadmansakibalam@gmail.com>
2024-06-09 22:16:29 +0600

    condition check

---
+++ cnditions.py
@@ -0,0 +1,12 @@
+#This side is for condition
+print("To check condition ")
+w= input("Please enter your waight: ")
+k= w
+l= float(w)*0.43
+a=input(" enter your your preference what do you want is it in Kilo or pound if kilo type k or for pound type l: ")
+if(a=='k'):
+    print(k)
+elif(a=='l'):
+    print(l)
+else:
+    print("please type currect key to see the result")
