013bff286da648c99a263f63e231a0ea1aa2186f
shadman76r <shadmansakibalam@gmail.com>
2024-07-04 16:04:21 +0600

    B

---
+++ practice.py
@@ -0,0 +1,14 @@
+#All Basic of Python for AI subject
+#1
+print("use any name here or text so no problem here")
+#2
+if 5>2:
+    print("Five is greater then Two!")
+#3
+if 5>2:
+    print("Five is greater then Two!")
+if 5>2:
+    print("Five is greater then Two!")
+#4
+x=5
+y="any name"    
